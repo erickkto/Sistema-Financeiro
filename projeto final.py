71d95ea43267b7ad137009bc930699ef95b50d1a
Joao Victor <joaovictor.benfica.23@gmail.com>
2026-05-19 21:10:04 -0300

    Calculadora top

--- projeto final.py
+++ projeto final.py
@@ -1,7 +1,21 @@
-print("Hello World!")
-a = 2
-b = 1
-c = a + b
-print(c)
+def operacoes():
+    opcao = int(input("Digite a operação desejada\n 1-Soma\n 2-Subtração\n 3-Multiplicação\n 4-divisao"))
+    if opcao == 1:
+        num1 = int(input("Digite o primeiro numero: "))
+        num2 = int(input("Digite o segundo número: "))
+        soma = num1 + num2
+        print (f"A soma dos números {num1} e {num2} é {soma}")
+    elif opcao == 2:
+        num1 = int (input("Digite o primeiro numero"))
+        num2 = int (input("Digite o segundo numero: "))
+        sub = num1 - num2
+        print (f"A subtração dos numeros {num1} e {num2} é {sub}")
 
-print (4)
+operacoes()
+
+decisao = input ("Deseja realizar outra operação?")
+
+if decisao == "Sim":
+    operacoes()
+else:
+    print ("Adeus!")
