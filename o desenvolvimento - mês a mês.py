0c451a129cbe88b5714a241177c3af397da1911f
Eduardo <duduaraujo.steigleder@gmail.com>
2026-05-26 22:37:34 -0300

    continuação

--- o desenvolvimento - mês a mês.py
+++ o desenvolvimento - mês a mês.py
@@ -1,7 +1,48 @@
-be = int(800) ##bolsa estágio 2000-1200, já descontando aluguel, internet e miojo
-sldi = int(1000) ##saldo inicial
+be = float(800) ##bolsa estágio 2000-1200, já descontando aluguel, internet e miojo
+sldi = float(1000) ##saldo inicial
+gasto= float(0)
 
-sldr = be + sldi
+sldr = be + sldi - gasto
 
-mês = int(input("Qual mês estamos? "))
-gasto = int(input("Quanto foi o gasto esse mês: "))
+mes = int(1)
+
+while True:
+    if mes == 1:
+        a = str(input("Escolha uma destas opções:\n a:"))
+        mes = mes + 1
+    elif mes == 2:
+        print("b")
+        mes = mes + 1
+    elif mes == 3:
+        print("c")
+        mes = mes + 1
+    elif mes == 4:
+        print("d")
+        mes = mes + 1
+    elif mes == 5:
+        print("e")
+        mes = mes + 1
+    elif mes == 6:
+        print("f")
+        mes = mes + 1
+    elif mes == 7:
+        print("g")
+        mes = mes + 1
+    elif mes == 8:
+        print("h")
+        mes = mes + 1
+    elif mes == 9:
+        print("i")
+        mes = mes + 1
+    elif mes == 10:
+        print("j")
+        mes = mes + 1
+    elif mes == 11:
+        print("k")
+        mes = mes + 1
+    elif mes == 12:
+        print("l")
+        mes = mes + 1
+    else:
+        print("fim")
+        break
